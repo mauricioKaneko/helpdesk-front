b2b4d310f9a28984501351b377c35c155a36919f
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-21 11:26:30 -0300

    Criando rota para lista de tecnico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TecnicoListComponent } from './component/tecnico/tecnico-list/tecnico-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
     path:'', component: NavComponent, children:[
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'tecnicos', component: TecnicoListComponent }
     ]
+
   }
 
 
