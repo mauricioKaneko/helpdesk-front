992095558d8fa90b3ab22278ecf54e2881db79a8
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-21 11:46:16 -0300

    listando tecnico

--- src/app/component/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/component/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Tecnico } from './../../../models/tecnico';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+  ELEMENT_DATA: Tecnico[] = [
+    {
+    id:1,
+    nome:"Kaneko",
+    cpf:"269.063.838-09",
+    email:"kaneko.kaneko@gmail.com",
+    senha:'12345',
+    perfis:['0'],
+    dataCriacao:'15/08/2022'
 
+    }
+  ]
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+ 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
